96a6ad3233196fc48a2db5207d39586696e92ad7
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-20 19:07:54 -0400

    Starting transition1

---
+++ App14Transition1/App14Transition1/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  App14Transition1
+//
+//  Created by Gabriel Gheorghian on 2014-09-20.
+//  Copyright (c) 2014 genunine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let container = UIView()
+    let redSquare = UIView()
+    let blueSquare = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set container frame and add to the screen
+        self.container.frame = CGRect(x: 60, y: 60, width: 200, height: 200)
+        self.view.addSubview(container)
+        
+        // set red square frame up
+        // we want the blue square to have the same position as redSquare
+        // so lets just reuse blueSquare.frame
+        self.redSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        self.blueSquare.frame = redSquare.frame
+        
+        // set background colors
+        self.redSquare.backgroundColor = UIColor.redColor()
+        self.blueSquare.backgroundColor = UIColor.blueColor()
+        
+        // for now just add the redSquare
+        // we'll add blueSquare as part of the transition animation
+        self.container.addSubview(self.redSquare)
+    }
+    
+}
+
