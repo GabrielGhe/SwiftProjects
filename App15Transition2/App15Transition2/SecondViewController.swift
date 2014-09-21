dafa484eb6c62980f3c56643dd1a9ee40a7e050b
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-09-21 09:36:48 -0400

    trying out navigation controller stuff

---
+++ App15Transition2/App15Transition2/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  App15Transition2
+//
+//  Created by Gabriel Gheorghian on 2014-09-21.
+//  Copyright (c) 2014 genunine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancel(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
